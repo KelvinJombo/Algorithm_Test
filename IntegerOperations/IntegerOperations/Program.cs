8b979474ba06cd2a171b94d0a989029b3feacb64
Kelvin Okonkwo <kelly2000s@yahoo.com>
2026-04-07 17:32:21 +0100

    Finished Solution

---
+++ IntegerOperations/IntegerOperations/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace IntegerOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var run = ArrayWorks([ 8, 8]);
+
+            Console.WriteLine(run);
+        }
+
+
+        private static int ArrayWorks(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0) 
+                {
+                    sum += 1;
+
+                    if (numbers[i] == 8)
+                    {
+                        sum += 5;
+                    }
+
+                }               
+                else
+                {
+                    sum += 3;
+                }
+            }
+
+            return sum;
+
+        }
+
+
+
+
+    }
+
+}
